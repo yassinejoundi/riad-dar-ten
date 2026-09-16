67589e86ed3abed391013d8ddd836064a9f2d0cd
Yassine Joundi <56726931+yassinejoundi@users.noreply.github.com>
2026-09-16 13:21:45 +0100

    feat: add visit and reservation details

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { DiningExperience } from "@/components/home/DiningExperience"
 import { HeroSection } from "@/components/home/HeroSection"
 import { RestaurantStory } from "@/components/home/RestaurantStory"
 import { SeasonalMenu } from "@/components/home/SeasonalMenu"
+import { VisitReservation } from "@/components/home/VisitReservation"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <RestaurantStory />
       <SeasonalMenu />
       <DiningExperience />
+      <VisitReservation />
     </div>
   )
 }
